ed0da9f4d333df700c431d8ed02617e1bb8a058e
3298421u849012 <144854854+3298421u849012@users.noreply.github.com>
2025-02-14 12:02:00 +0000

    exercicio 3

--- App.java
+++ App.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Bem-vindo, " + nome + "!");
-        scanner.close();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite um número: ");
+            int num = scanner.nextInt();
+            if (num % 2 == 0) {
+            System.out.println("O número " + num + " é par.");
+            } else {
+            System.out.println("O número " + num + " é ímpar.");
+            }
+            scanner.close();
+           
     }
 }
